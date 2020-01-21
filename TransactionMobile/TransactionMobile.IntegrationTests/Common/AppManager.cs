e407008c2aefdcb2a3ba624dc0778e6671f86302
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-01-21 08:26:25 +0000

    :|

--- TransactionMobile/TransactionMobile.IntegrationTests/Common/AppManager.cs
+++ TransactionMobile/TransactionMobile.IntegrationTests/Common/AppManager.cs
@@ -10,6 +10,7 @@ using Xamarin.UITest.Queries;
 
 namespace TransactionMobile.IntegrationTests.Common
 {
+    using System.Threading;
     using NUnit.Framework;
 
     static class AppManager
@@ -134,11 +135,17 @@ namespace TransactionMobile.IntegrationTests.Common
         /// Verifies that the trait is present. Uses the default wait time.
         /// </summary>
         /// <param name="timeout">Time to wait before the assertion fails</param>
-        public void AssertOnPage(TimeSpan? timeout = default(TimeSpan?))
+        public async Task AssertOnPage(TimeSpan? timeout = default(TimeSpan?))
         {
-            var message = "Unable to verify on page: " + this.GetType().Name;
-            
-            Assert.DoesNotThrow(() => app.WaitForElement(Trait.Current, timeout: timeout), message);
+            await Retry.For(async () =>
+                            {
+                                var message = "Unable to verify on page: " + this.GetType().Name;
+
+                                Assert.DoesNotThrow(() => app.WaitForElement(Trait.Current, timeout:timeout), message);
+                            },
+                            TimeSpan.FromMinutes(1),
+                            timeout).ConfigureAwait(false);
+
         }
 
         /// <summary>
@@ -166,5 +173,68 @@ namespace TransactionMobile.IntegrationTests.Common
         {
             AppManager.StartApp();
         }
+    }
+
+    public static class Retry
+    {
+        #region Fields
+
+        /// <summary>
+        /// The default retry for
+        /// </summary>
+        private static readonly TimeSpan DefaultRetryFor = TimeSpan.FromSeconds(60);
+
+        /// <summary>
+        /// The default retry interval
+        /// </summary>
+        private static readonly TimeSpan DefaultRetryInterval = TimeSpan.FromSeconds(5);
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Fors the specified action.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <param name="retryFor">The retry for.</param>
+        /// <param name="retryInterval">The retry interval.</param>
+        /// <returns></returns>
+        public static async Task For(Func<Task> action,
+                                     TimeSpan? retryFor = null,
+                                     TimeSpan? retryInterval = null)
+        {
+            DateTime startTime = DateTime.Now;
+            Exception lastException = null;
+
+            if (retryFor == null)
+            {
+                retryFor = Retry.DefaultRetryFor;
+            }
+
+            while (DateTime.Now.Subtract(startTime).TotalMilliseconds < retryFor.Value.TotalMilliseconds)
+            {
+                try
+                {
+                    await action().ConfigureAwait(false);
+                    lastException = null;
+                    break;
+                }
+                catch (Exception e)
+                {
+                    lastException = e;
+
+                    // wait before retrying
+                    Thread.Sleep(retryInterval ?? Retry.DefaultRetryInterval);
+                }
+            }
+
+            if (lastException != null)
+            {
+                throw lastException;
+            }
+        }
+
+        #endregion
     }
 }
