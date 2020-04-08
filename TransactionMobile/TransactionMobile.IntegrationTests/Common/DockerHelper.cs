0a4a1b0627604da420b19ad9a67f8383732f8d76
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-04-08 10:08:30 +0100

    :|

--- TransactionMobile/TransactionMobile.IntegrationTests/Common/DockerHelper.cs
+++ TransactionMobile/TransactionMobile.IntegrationTests/Common/DockerHelper.cs
@@ -859,13 +859,13 @@ namespace TransactionMobile.IntegrationTests.Common
             Int32 maxRetries = 10;
             Int32 counter = 1;
 
-            String localhostaddress = Environment.GetEnvironmentVariable("localhostaddress");
-            if (String.IsNullOrEmpty(localhostaddress))
-            {
-                localhostaddress = "192.168.1.67";
-            }
+            //String localhostaddress = Environment.GetEnvironmentVariable("localhostaddress");
+            //if (String.IsNullOrEmpty(localhostaddress))
+            //{
+            //    localhostaddress = "192.168.1.67";
+            //}
 
-            String server = localhostaddress;
+            String server = "127.0.0.1";
             String database = "SubscriptionServiceConfiguration";
             String user = sqlUserName;
             String password = sqlPassword;
