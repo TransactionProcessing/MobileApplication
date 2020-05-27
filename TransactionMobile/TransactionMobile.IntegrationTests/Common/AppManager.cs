c7177c7a56892c5a5596a04dfd14077dae32389f
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-05-27 17:01:47 +0100

    :|

--- TransactionMobile/TransactionMobile.IntegrationTests/Common/AppManager.cs
+++ TransactionMobile/TransactionMobile.IntegrationTests/Common/AppManager.cs
@@ -77,7 +77,7 @@ namespace TransactionMobile.IntegrationTests.Common
 
             if (Platform == Platform.iOS)
             {
-                String deviceIdentifier = AppManager.GetDeviceIdentifier("iPhone 11");
+                String deviceIdentifier = AppManager.GetDeviceIdentifier("iPhone 11 (13.5)");
 
                 String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", @"TransactionMobile.iOS/bin/iPhoneSimulator/Release");
                 app = ConfigureApp
