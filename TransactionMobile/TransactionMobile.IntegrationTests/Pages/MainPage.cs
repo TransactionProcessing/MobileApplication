6a930fde2af0694223a26eaf6f9373f1ee6b561d
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-06-20 06:43:21 +0100

    .

--- TransactionMobile/TransactionMobile.IntegrationTests/Pages/MainPage.cs
+++ TransactionMobile/TransactionMobile.IntegrationTests/Pages/MainPage.cs
@@ -101,7 +101,7 @@
 
             //await Task.Delay(TimeSpan.FromSeconds(60));
 
-            //AppManager.App.Print.Tree();
+            AppManager.App.Print.Tree();
 
             return balanceValue;
         }
