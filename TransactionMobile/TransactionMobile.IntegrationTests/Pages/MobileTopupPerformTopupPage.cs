97798f29af856ac1ff69506326f0fbf0bf59851e
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-11-07 08:42:52 +0000

    :|

--- TransactionMobile/TransactionMobile.IntegrationTests/Pages/MobileTopupPerformTopupPage.cs
+++ TransactionMobile/TransactionMobile.IntegrationTests/Pages/MobileTopupPerformTopupPage.cs
@@ -66,7 +66,7 @@
         public void AssertTopupValidationErrorDisplayed()
         {
             String errorMessage = "Please enter a mobile number and Topup Amount to continue";
-            app.WaitForElement(errorMessage, timeout:TimeSpan.FromSeconds(45));
+            app.WaitForElement(errorMessage, timeout:TimeSpan.FromMinutes(2));
             
             // Dismiss the error
             app.Tap("OK");
