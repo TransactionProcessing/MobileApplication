638b8bb5e49028549ca981082244e4b5e940e181
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-11-10 08:13:27 +0000

    :|

---
+++ TransactionMobile/TransactionMobile.IntegrationTests/Script/runtests.sh
@@ -0,0 +1,3 @@
+﻿#!/bin/sh
+
+mono TransactionMobile/packages/NUnit.ConsoleRunner.3.11.1/tools/nunit3-console.exe TransactionMobile/TransactionMobile.IntegrationTests/bin/Release/TransactionMobile.IntegrationTests.dll --where "cat == Android"
