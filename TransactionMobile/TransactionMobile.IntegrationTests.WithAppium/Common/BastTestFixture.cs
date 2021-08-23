b9325464d79d61af05bcc7fc968cadb446b9eb56
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-08-23 12:04:27 +0100

    ..

--- TransactionMobile/TransactionMobile.IntegrationTests.WithAppium/Common/BastTestFixture.cs
+++ TransactionMobile/TransactionMobile.IntegrationTests.WithAppium/Common/BastTestFixture.cs
@@ -29,7 +29,7 @@ namespace TransactionMobile.IntegrationTests.WithAppium.Features
     using NUnit.Framework;
 
     //[TestFixture(MobileTestPlatform.Android, Category = "Android")]
-    //[TestFixture(MobileTestPlatform.iOS, Category = "iOS")]
+    [TestFixture(MobileTestPlatform.iOS, Category = "iOS")]
     public partial class LoginFeature : BaseTestFixture
     {
         public LoginFeature(MobileTestPlatform mobileTestPlatform)
@@ -39,7 +39,7 @@ namespace TransactionMobile.IntegrationTests.WithAppium.Features
     }
 
     //[TestFixture(MobileTestPlatform.Android, Category = "Android")]
-    //[TestFixture(MobileTestPlatform.iOS, Category = "iOS")]
+    [TestFixture(MobileTestPlatform.iOS, Category = "iOS")]
     public partial class SafaricomTopupFeature : BaseTestFixture
     {
         public SafaricomTopupFeature(MobileTestPlatform platform)
@@ -49,7 +49,7 @@ namespace TransactionMobile.IntegrationTests.WithAppium.Features
     }
 
     //[TestFixture(MobileTestPlatform.Android, Category = "Android")]
-    //[TestFixture(MobileTestPlatform.iOS, Category = "iOS")]
+    [TestFixture(MobileTestPlatform.iOS, Category = "iOS")]
     public partial class AdminFeature : BaseTestFixture
     {
         public AdminFeature(MobileTestPlatform platform)
@@ -59,7 +59,7 @@ namespace TransactionMobile.IntegrationTests.WithAppium.Features
     }
 
     //[TestFixture(MobileTestPlatform.Android, Category = "Android")]
-    //[TestFixture(MobileTestPlatform.iOS, Category = "iOS")] 
+    [TestFixture(MobileTestPlatform.iOS, Category = "iOS")] 
     public partial class VoucherIssueFeature : BaseTestFixture
     {
         public VoucherIssueFeature(MobileTestPlatform platform)
