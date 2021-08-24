c76928fb34da268fd32b7c6190f5b2a114977938
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-08-24 21:20:41 +0100

    another option

--- TransactionMobile/TransactionMobile.iOS/AppDelegate.cs
+++ TransactionMobile/TransactionMobile.iOS/AppDelegate.cs
@@ -90,13 +90,13 @@
             SfButtonRenderer.Init();
             SfTabViewRenderer.Init();
 
-            var x =  NetworkInterface.GetAllNetworkInterfaces()
-                                   .Where(ni => ni.Name.Equals("en0"))
-                                   .First().GetIPProperties().UnicastAddresses
-                                   .Where(add => add.Address.AddressFamily == AddressFamily.InterNetwork)
-                                   .First().Address.ToString();
+            //var x =  NetworkInterface.GetAllNetworkInterfaces()
+            //                       .Where(ni => ni.Name.Equals("en0"))
+            //                       .First().GetIPProperties().UnicastAddresses
+            //                       .Where(add => add.Address.AddressFamily == AddressFamily.InterNetwork)
+            //                       .First().Address.ToString();
             // Initialize the MQTT backdoor
-            Task t = Backdoor.Instance.Initialize(mqttHost: x);
+            Task t = Backdoor.Instance.Initialize();
 
             // Handle backdoor events
             Backdoor.Instance.BackdoorEvent += HandleBackdoorEvent;
