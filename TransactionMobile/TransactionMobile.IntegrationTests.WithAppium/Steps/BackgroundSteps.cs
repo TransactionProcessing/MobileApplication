940ec91db7a9f49ba514b46d058e956e16468a77
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-11-09 22:06:21 +0000

    ..

--- TransactionMobile/TransactionMobile.IntegrationTests.WithAppium/Steps/BackgroundSteps.cs
+++ TransactionMobile/TransactionMobile.IntegrationTests.WithAppium/Steps/BackgroundSteps.cs
@@ -207,11 +207,11 @@ namespace TransactionMobile.IntegrationTests.WithAppium.Steps
                 var merchantData = JsonConvert.SerializeObject(m);
                 var c = this.TestingContext.GetContracts();
                 var contractData = JsonConvert.SerializeObject(c);
-                await this.TestModePage.EnterPin("1234");
+                //await this.TestModePage.EnterPin("1234");
                 
-                await this.TestModePage.EnterTestContractData(contractData);
-                await this.TestModePage.EnterTestMerchantData(merchantData);
-                await this.TestModePage.ClickSetTestModeButton();
+                //await this.TestModePage.EnterTestContractData(contractData);
+                //await this.TestModePage.EnterTestMerchantData(merchantData);
+                //await this.TestModePage.ClickSetTestModeButton();
             }
         }
     }
